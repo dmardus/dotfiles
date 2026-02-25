56c6af76dd8d47e80c2fa927ca54cec3bd22d8f7
dmardus <44878982+dmardus@users.noreply.github.com>
2026-02-25 12:48:45 -0700

    Fixed the installation process for nushell under Fedora

--- scripts/fedora/install-nushell.sh
+++ scripts/fedora/install-nushell.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 set -e
+
+echo "[*] Setting up Gemfury Nushell repo..."
+sudo tee /etc/yum.repos.d/fury-nushell.repo > /dev/null <<EOF
+[gemfury-nushell]
+name=Gemfury Nushell Repo
+baseurl=https://yum.fury.io/nushell/
+enabled=1
+gpgcheck=0
+gpgkey=https://yum.fury.io/nushell/gpg.key
+EOF
+
 echo "[*] Installing NuShell..."
 sudo dnf install -y nushell
+
 echo "[✓] NuShell installed."

c486ef1b0172053f6fb717e02f9ce79a36116668
dmardus <44878982+dmardus@users.noreply.github.com>
2026-02-25 13:19:56 -0700

    Fixed Zen browser installation for Fedora

--- scripts/fedora/install-zen.sh
+++ scripts/fedora/install-zen.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -e
 echo "[*] Installing Zen Browser..."
-sudo dnf config-manager --add-repo https://repo.zen-browser.app/rpm
-sudo rpm --import https://repo.zen-browser.app/zen-browser.asc
+sudo dnf copr enable sneexy/zen-browser
 sudo dnf install -y zen-browser
 echo "[✓] Zen Browser installed."

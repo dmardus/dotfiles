7ae60c265701c1d9a26ea9a3e4cac93235d17737
dmardus <44878982+dmardus@users.noreply.github.com>
2026-02-25 13:09:40 -0700

    Fixed the installation of windsurf for Fedora

--- scripts/fedora/install-windsurf.sh
+++ scripts/fedora/install-windsurf.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -e
 echo "[*] Installing windsurf..."
+sudo rpm --import https://windsurf-stable.codeiumdata.com/wVxQEIWkwPUEAGf3/yum/RPM-GPG-KEY-windsurf
+echo -e "[windsurf]\nname=Windsurf Repository\nbaseurl=https://windsurf-stable.codeiumdata.com/wVxQEIWkwPUEAGf3/yum/repo/\nenabled=1\nautorefresh=1\ngpgcheck=1\ngpgkey=https://windsurf-stable.codeiumdata.com/wVxQEIWkwPUEAGf3/yum/RPM-GPG-KEY-windsurf" | sudo tee /etc/yum.repos.d/windsurf.repo > /dev/null
+sudo dnf check-update
 sudo dnf install -y windsurf
 echo "[✓] windsurf installed."
